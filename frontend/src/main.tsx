acb0ecb2e2b06cbb1e18a07deb87fc6b98c75027
Young <iamlovedit@163.com>
2025-02-18 22:55:36 +0800

    configured project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+"use client";
 
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { HeroUIProvider } from "@heroui/react";
+
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HeroUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <App />
+      </NextThemesProvider>
+    </HeroUIProvider>
+  </StrictMode>
+);
